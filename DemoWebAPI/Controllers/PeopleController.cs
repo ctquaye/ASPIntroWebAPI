e8d19026610117a2d2a5745b98ab828a969ff900
ctquaye <ctquaye@gmail.com>
2020-08-02 23:34:56 -0500

    API's documentation

--- DemoWebAPI/Controllers/PeopleController.cs
+++ DemoWebAPI/Controllers/PeopleController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DemoWebAPI.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -7,29 +8,61 @@ using System.Web.Http;
 
 namespace DemoWebAPI.Controllers
 {
+    /// <summary>
+    /// The following will give you details about the people API
+    /// </summary>
     public class PeopleController : ApiController
     {
-        // GET: api/People
-        public IEnumerable<string> Get()
+
+        List<Person> people = new List<Person>();
+        /// <summary>
+        /// Initial values of the people object
+        /// </summary>
+        public PeopleController()
         {
-            return new string[] { "value1", "value2" };
+            people.Add(new Person { FirstName = "Tim", LastName = "Corey", Id = 1 });
+            people.Add(new Person { FirstName = "Jeff", LastName = "Otoo-Larh", Id = 2 });
+            people.Add(new Person { FirstName = "Ataa", LastName = "Mensah", Id = 3 });
+        }
+
+        /// <summary>
+        /// Gets a list of the first names of all users.
+        /// </summary>
+        /// <param name="userId">The unique identifier for this person</param>
+        /// <param name="age">How old this person is</param>
+        /// <returns>list of first names</returns>
+        [Route("api/People/GetFirstNames/{userId:int}/{age:int}")]
+        [HttpGet]
+        public List<string>GetFirstNames(int userId, int age)
+        {
+            List<string> output = new List<string>();
+
+            foreach (var p in people)
+            {
+                output.Add(p.FirstName);
+            }
+            return output;
+
+        }
+
+        // GET: api/People
+        public List<Person> Get()
+        {
+            return people;
         }
 
         // GET: api/People/5
-        public string Get(int id)
+        public Person Get(int id)
         {
-            return "value";
+            return people.Where(x => x.Id == id).FirstOrDefault();
         }
 
         // POST: api/People
-        public void Post([FromBody]string value)
+        public void Post(Person value)
         {
+            people.Add(value);
         }
 
-        // PUT: api/People/5
-        public void Put(int id, [FromBody]string value)
-        {
-        }
 
         // DELETE: api/People/5
         public void Delete(int id)
---
+++ DemoWebAPI/Models/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DemoWebAPI.Models
+{
+    /// <summary>
+    /// Represents one specific person
+    /// </summary>
+    public class Person
+    {
+        /// <summary>
+        /// ID from Sql
+        /// </summary>
+        public int Id { get; set; } = 0;
+        /// <summary>
+        /// User's first name
+        /// </summary>
+        public string FirstName { get; set; } = "";
+        /// <summary>
+        /// User's last name
+        /// </summary>
+        public string LastName { get; set; } = "";
+    }
+}
